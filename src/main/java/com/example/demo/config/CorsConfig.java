b4dd5df94a740aa36e89371748f5ac74585e4ae9
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-06-02 12:21:22 +0900

    fix: [남현기] CorsConfig IP 수정

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:8080",
                         "http://localhost:8080",
-                        "http://15.165.209.91", //현기
+                        "http://3.36.251.74", //현기
                         "http://43.201.44.74" //여울
                  )
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
